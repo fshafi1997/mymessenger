724caa65667b0c3a988daf883effb32babe64440
Farhan Shafi <fshafi@purdue.edu>
2018-04-06 15:42:40 -0400

    added connection to database

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
         <title>Random Number</title>
     </head>
     <body>
-        <p>Generating a random number between 1 and 10:
+        <p>Testing if the database is connected
             <?php
+            /*param1=db host
+            param2=username
+            param3=password
+            param4=db name*/
+            $db = new mysqli("localhost","fshafi","y92Sle6&","fshafi_messages");
 
-            echo rand(1, 10);
-
+            if($db->connect_error){
+                die("Was unable to connect to database");
+            } else {
+            die("Connection to database was successfull");
+            }
+            
             ?>
         </p>
     </body>
