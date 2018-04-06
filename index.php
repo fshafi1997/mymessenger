a82142da54d72eb6222698d3d53e8397d7feeb29
Farhan Shafi <fshafi@purdue.edu>
2018-04-06 13:46:03 -0400

    testing the connection with database

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+<!--
+param1=db host
+param2=username
+param3=password
+param4=db name-->
+
+$db = new mysqli("localhost","fshafi","uWym1?64","fshafi_messages");
+
+if($db->connect_error){
+    die("Was unable to connect to database");
+} else {
+    die("Connection to database was successfull");
+}
