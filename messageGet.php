6b1b45d1b3f09c8521e5bd7744f2a5b77c17dcf8
Farhan Shafi <fshafi@purdue.edu>
2018-04-10 16:18:46 -0400

    added the basic page layout

---
+++ messageGet.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CS252 Lab-6</title>
+    </head>
+    <body>
+        <p>Testing if the database is connected<br />
+            <?php
+            /*param1=db host
+            param2=username
+            param3=password
+            param4=db name*/
+            $db = new mysqli("localhost","fshafi","y92Sle6&","fshafi_messages");
+
+            if($db->connect_error){
+                die("Was unable to connect to database");
+            } else {
+            die("Connection to database was successfull");
+            }
+            
+            ?>
+        </p>
+    </body>
+</html>
