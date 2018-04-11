9538a4985aabb217b376643445c9555386a08c0f
Farhan Shafi <fshafi@purdue.edu>
2018-04-11 15:22:05 -0400

    added db config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect("localhost:3306","fshafi","y92Sle6&","fshafi_messages");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+} else echo "done connection"
+?>
