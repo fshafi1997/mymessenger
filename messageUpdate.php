e825b7597ac8284ff5b657ac04346d9886d15eda
Farhan Shafi <fshafi@purdue.edu>
2018-04-10 21:42:13 -0400

    testing

--- messageUpdate.php
+++ messageUpdate.php
@@ -14,22 +14,24 @@ else echo "connected to the data base";
 // whatever set in environment variable will come here
 // using htmlspecialchars to prevent users from putting tags in url
 // strip slashes used to clean up data remove  \
-$username = stripslashes(htmlspecialchars($_GET['username']));
-$message = stripslashes(htmlspecialchars($_GET['message']));
+$username = $_GET['username'];
+echo $username;
+//$message = stripslashes(htmlspecialchars($_GET['message']));
 
+/*
 // if username or message is empty exit current script
 if ($message == "" || $username == "") {
 	die();
 }
 
-/*// insert the message into the data base
+// insert the message into the data base
 // 1st value id(auto incremented) 2nd value (username) 3rd value (message)
 $result = $db->prepare("INSERT INTO messages VALUES('',?,?)");
 // substituting the question mark "ss" for string username and string message
 $result->bind_param("ss", $username, $message);
 echo "username is $username\n";
 echo "message is $message\n";
-$result->execute();*/
+$result->execute();
 
 
 $sql = "INSERT INTO messages (id, username, message) VALUES ('', $username, $message)";
@@ -42,6 +44,6 @@ if ($db->query($sql) === TRUE) {
     echo "Error: " . $db . "<br>" . $db->error;
 }
 
-$db->close();
+$db->close();*/
 
 ?>
