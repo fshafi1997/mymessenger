821c4010016f4e6ffef039c5f1ac886682611f67
Farhan Shafi <fshafi@purdue.edu>
2018-04-10 22:09:50 -0400

    insertion of messages not working

--- messageUpdate.php
+++ messageUpdate.php
@@ -4,22 +4,29 @@
 param2=username
 param3=password
 param4=db name*/
-$db = new mysqli("localhost","fshafi","y92Sle6&","fshafi_messages");
+$db = new mysqli_connect("localhost:3306","fshafi","y92Sle6&","fshafi_messages");
 
-if($db->connect_error){
+/*if($db->connect_error){
     die("Was unable to connect to database". $db->connect_error);
 }
-else echo "connected to the data base","\n";
+else echo "connected to the data base","\n";*/
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+    echo "<br>";
+}
+
+echo "Connected successfully";
+echo "<br>";
 
 // whatever set in environment variable will come here
 // using htmlspecialchars to prevent users from putting tags in url
 // strip slashes used to clean up data remove  \
 $username = $_GET['username'];
 echo "$username";
-echo "";
+echo "<br>";
 $message = $_GET['message'];
 echo "$message";
-echo "";
+echo "<br>";
 
 
 
@@ -28,7 +35,16 @@ if ($message == "" || $username == "") {
 	die();
 }
 
-// insert the message into the data base
+$sql = "INSERT INTO messages (id, username, message) VALUES ('', $username, $message)";
+if (mysqli_query($db, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($db);
+
+
+/*// insert the message into the data base
 // 1st value id(auto incremented) 2nd value (username) 3rd value (message)
 $result = $db->prepare("INSERT INTO messages VALUES('',?,?)");
 // substituting the question mark "ss" for string username and string message
@@ -37,6 +53,6 @@ echo "username going in data base is $username";
 echo "";
 echo "message going in data base is $message";
 echo "";
-$result->execute();
+$result->execute();*/
 
 ?>
