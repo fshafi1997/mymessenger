a84256ce84e9ac7a764b8589490d51ffb54016b4
Farhan Shafi <fshafi@purdue.edu>
2018-04-10 21:52:00 -0400

    messages are not going in database

--- messageUpdate.php
+++ messageUpdate.php
@@ -9,15 +9,15 @@ $db = new mysqli("localhost","fshafi","y92Sle6&","fshafi_messages");
 if($db->connect_error){
     die("Was unable to connect to database". $db->connect_error);
 }
-else echo "connected to the data base\n";
+else echo "connected to the data base","\n";
 
 // whatever set in environment variable will come here
 // using htmlspecialchars to prevent users from putting tags in url
 // strip slashes used to clean up data remove  \
 $username = $_GET['username'];
-echo "$username\n";
+echo "$username","\n";
 $message = $_GET['message'];
-echo "$message\n";
+echo "$message","\n";
 
 
 // if username or message is empty exit current script
@@ -27,24 +27,24 @@ if ($message == "" || $username == "") {
 
 // insert the message into the data base
 // 1st value id(auto incremented) 2nd value (username) 3rd value (message)
-$result = $db->prepare("INSERT INTO messages VALUES('',?,?)");
+/*$result = $db->prepare("INSERT INTO messages VALUES('',?,?)");
 // substituting the question mark "ss" for string username and string message
 $result->bind_param("ss", $username, $message);
-echo "username is $username\n";
-echo "message is $message\n";
-$result->execute();
+echo "username $username";
+echo "message is $message";
+$result->execute();*/
 
 
-//$sql = "INSERT INTO messages (id, username, message) VALUES ('', $username, $message)";
+$sql = "INSERT INTO messages (id, username, message) VALUES ('', $username, $message)";
 
-/*if ($db->query($sql) === TRUE) {
+if ($db->query($sql) === TRUE) {
     echo "New record created successfully\n";
-    echo "username is $username\n";
-    echo "message is $message\n";
+    echo "username gone in db is $username","\n";
+    echo "message gone in db is $message","\n";
 } else {
     echo "Error: " . $db . "<br>" . $db->error;
-}*/
+}
 
-//$db->close();
+$db->close();
 
 ?>
