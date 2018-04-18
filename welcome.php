45316e274123c75330cd349be8d0d0cbb77292c1
Farhan Shafi <fshafi@purdue.edu>
2018-04-18 14:46:19 -0400

    reduced time interval of calling update

--- welcome.php
+++ welcome.php
@@ -299,7 +299,7 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 
 		}
 		// for auto updating
-		setInterval(function(){ update() }, 2500);
+		setInterval(function(){ update() }, 1500);
 	</script>
 </body>
 </html>
