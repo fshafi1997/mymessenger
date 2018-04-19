d2fe9cd0ccd96ba15b934a00dbcc45eacb4f39d5
Farhan Shafi <fshafi@purdue.edu>
2018-04-18 20:05:12 -0400

    background to register page

--- register.php
+++ register.php
@@ -105,7 +105,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
-<body>
+<body background="backgroundW.jpg">
     <div class="wrapper" style="margin: 0px auto;">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
