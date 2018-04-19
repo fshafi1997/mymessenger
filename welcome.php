80156a9675a4658c71e788b16555e752424cecf2
Farhan Shafi <fshafi@purdue.edu>
2018-04-18 23:12:46 -0400

    added a popup to show error

--- welcome.php
+++ welcome.php
@@ -177,6 +177,90 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
             float: right;
             margin-right: 12px;
         }
+
+        .popup {
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        /* The actual popup */ 
+        .popup .popuptext {
+            visibility: hidden;
+            width: 160px;
+            background-color: #cc2208;
+            color: #fff;
+            text-align: center;
+            border-radius: 6px;
+            padding: 8px 0;
+            position: absolute;
+            z-index: 1;
+            bottom: 125%;
+            left: 50%;
+            margin-left: -80px;
+        }
+
+        /* Popup arrow */
+        .popup .popuptext::after {
+            content: "";
+            position: absolute;
+            top: 100%;
+            left: 50%;
+            margin-left: -5px;
+            border-width: 5px;
+            border-style: solid;
+            border-color: #555 transparent transparent transparent;
+        }
+
+        /* to hide or show pop up */
+        .popup .show {
+            visibility: visible;
+            -webkit-animation: fadeIn 1s;
+            animation: fadeIn 1s;
+        }
+
+        /* to hide the popup */
+        #hideMe {
+        -moz-animation: cssAnimation 0s ease-in 5s forwards;
+        /* Firefox */
+        -webkit-animation: cssAnimation 0s ease-in 5s forwards;
+        /* Safari and Chrome */
+        -o-animation: cssAnimation 0s ease-in 5s forwards;
+        /* Opera */
+        animation: cssAnimation 0s ease-in 5s forwards;
+        -webkit-animation-fill-mode: forwards;
+        animation-fill-mode: forwards;
+        }
+
+        @keyframes cssAnimation {
+            to {
+                width:0;
+                height:0;
+                overflow:hidden;
+            }
+        }
+        @-webkit-keyframes cssAnimation {
+            to {
+                width:0;
+                height:0;
+                visibility:hidden;
+            }
+        }
+
+        /* Add animation (fade in the popup) */
+        @-webkit-keyframes fadeIn {
+            from {opacity: 0;} 
+            to {opacity: 1;}
+        }
+
+        @keyframes fadeIn {
+            from {opacity: 0;}
+            to {opacity:1 ;}
+        }
     </style>
 </head>
 <body onload="checkcookie(); update();">
@@ -223,9 +307,16 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
             document.cookie = "messengerUname=" + user;
             checkcookie();
         } else {
+            <div class="popup" id='hideMe' onload="popupFunc()">
+            <span class="popuptext" id="myPopup">Must enter a username to continue</span>
+            </div>
             showlogin();
         }
+    }
 
+    function popupFunc() {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
     }
 
     // login show method
