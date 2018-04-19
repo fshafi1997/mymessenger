30dbf49872ba64da66d0b1953d70b59f9d11362b
Farhan Shafi <fshafi@purdue.edu>
2018-04-18 20:52:34 -0400

    removed extra echo

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ $link = mysqli_connect("localhost:3306","fshafi","y92Sle6&","fshafi_messages");
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-} else echo "done connection"
+}
 ?>
